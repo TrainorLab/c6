775f2a9e3fc013ef50829dc8b2c917438250c70e
Dobromir Dotov <dobri.dotov@gmail.com>
2021-09-21 12:33:48 -0400

    tune to graham

--- step_2_train.py
+++ step_2_train.py
@@ -10,5 +10,5 @@ path_config_folder = os.getcwd()
 print(path_config_folder)
 path_config_file = os.path.join(path_config_folder,'config.yaml')
 
-deeplabcut.create_training_dataset(path_config_file,windows2linux=TRUE)
-deeplabcut.train_network(path_config_file, shuffle=1, saveiters=50000, displayiters=10000)
+deeplabcut.create_training_dataset(path_config_file,windows2linux=True)
+deeplabcut.train_network(path_config_file, shuffle=1, saveiters=50000, displayiters=10000, maxiters=50000) # maxiters=50000 or even less for shorter, demo purposes.
